86d1a07563ba8847f305cb6d7dfe7c03f58f6f01
uzairashraf <faisal.khanani.75@gmail.com>
2016-04-18 10:40:59 +0500

    pull to start 18 april 2016 work

--- Database/migrate/041_Script.sql
+++ Database/migrate/041_Script.sql
@@ -2,5 +2,6 @@ BEGIN_SETUP:
 
 alter table tbBank alter column startdate [datetime] null
 alter table tbBank alter column enddate [datetime] null
+alter table tbReceipt drop column CurrencyCode
 
 END_SETUP:
