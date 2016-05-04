b24af2adb6d8f9c885eff934f609492e10a2fce3
uzairashraf <faisal.khanani.75@gmail.com>
2016-05-04 10:36:15 +0500

    Shipment backend partial

---
+++ 360Accounting.Core/IService/IShipmentService.cs
@@ -0,0 +1,18 @@
+﻿using _360Accounting.Core.Entities;
+using _360Accounting.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _360Accounting.Core
+{
+    public interface IShipmentService : IService<Shipment>
+    {
+        IEnumerable<Shipment> GetAll(long companyId, long sobId);
+        IEnumerable<Shipment> GetAllByOrderId(long companyId, long sobId, long orderId);
+        IEnumerable<Shipment> GetAllWarehouseId(long companyId, long sobId, long warehouseId);
+        IEnumerable<Shipment> GetAllByLocatorId(long companyId, long sobId, long locatorId);
+    }
+}
---
+++ 360Accounting.Service/Services/ShipmentService.cs
@@ -0,0 +1,71 @@
+﻿using _360Accounting.Core;
+using _360Accounting.Core.Entities;
+using _360Accounting.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _360Accounting.Service
+{
+    public class ShipmentService : IShipmentService
+    {
+        private IShipmentRepository repository;
+
+        public ShipmentService(IShipmentRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        public IEnumerable<Shipment> GetAll(long companyId, long sobId)
+        {
+            return this.repository.GetAll(companyId, sobId);
+        }
+
+        public IEnumerable<Shipment> GetAllByOrderId(long companyId, long sobId, long orderId)
+        {
+            return this.repository.GetAllByOrderId(companyId, sobId, orderId);
+        }
+
+        public IEnumerable<Shipment> GetAllWarehouseId(long companyId, long sobId, long warehouseId)
+        {
+            return this.repository.GetAllByOrderId(companyId, sobId, warehouseId);
+        }
+
+        public IEnumerable<Shipment> GetAllByLocatorId(long companyId, long sobId, long locatorId)
+        {
+            return this.repository.GetAllByOrderId(companyId, sobId, locatorId);
+        }
+
+        public Shipment GetSingle(string id, long companyId)
+        {
+            return this.repository.GetSingle(id, companyId);
+        }
+
+        public IEnumerable<Shipment> GetAll(long companyId)
+        {
+            return this.repository.GetAll(companyId);
+        }
+
+        public string Insert(Shipment entity)
+        {
+            return this.repository.Insert(entity);
+        }
+
+        public string Update(Shipment entity)
+        {
+            return this.repository.Update(entity);
+        }
+
+        public void Delete(string id, long companyId)
+        {
+            this.repository.Delete(id, companyId);
+        }
+
+        public int Count(long companyId)
+        {
+            return this.repository.Count(companyId);
+        }
+    }
+}
